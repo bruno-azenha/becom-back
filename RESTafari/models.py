d6c9875b3f94e25f3cac6126707a5a4d4006e66f
- <azenha.bruno@gmail.com>
2015-09-28 23:03:36 -0300

    fixes

--- RESTafari/models.py
+++ RESTafari/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class HelloClass(models.Model):
-	HelloString = CharField(max_length=100)
+	HelloString = models.CharField(max_length=100)
 
