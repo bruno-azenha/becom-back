0fd9c065b7491a317762de62df809848f3b0d08f
Guilherme <gperinazzo@gmail.com>
2015-10-28 17:50:41 -0200

    Removed dangling reference

--- RESTafari/admin.py
+++ RESTafari/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import HelloClass
+from .models import Beacom
 
 # Register your models here.
-admin.site.register(HelloClass)
+admin.site.register(Beacom)
