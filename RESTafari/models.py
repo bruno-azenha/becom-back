8eb870253af6b4c689f8eae21f6d9ba8e73d25a7
- <azenha.bruno@gmail.com>
2015-11-09 23:03:12 -0200

    Fix small typos

--- RESTafari/models.py
+++ RESTafari/models.py
@@ -13,9 +13,9 @@ class Video(models.Model):
 
 class User(models.Model):
 	email  = models.EmailField()
-	name   = models.CharField(100)
-	age    = models.IntegerField(null=true)
-	gender = models.IntegerField(null=true)        
+	name   = models.CharField(max_length=100)
+	age    = models.IntegerField(null=True)
+	gender = models.IntegerField(null=True)        
 
 class Beacon(models.Model):
 	user            = models.ForeignKey(User)
